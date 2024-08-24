02141dad8c9faf22f667b9f7d9216a9b85dec33f
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2024-08-24 23:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const age = document.getElementById('age').value;
+  const name = document.getElementById('name').value;
+
+  if (age === '' || name === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  });
+
+  promise.then((name) => {
+    alert(`Welcome, ${name}. You can vote.`);
+  }).catch((name) => {
+    alert(`Oh sorry ${name}. You aren't old enough`);
+  });
+});
